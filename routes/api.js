ffafa9d8fd424e3ff21ffde2a0e924d1e9df53e3
Marcos Gual <111923@tecnicatura.frc.utn.edu.ar>
2022-06-27 12:34:28 -0300

    actualizacion api.js

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,29 @@
-const express = require('express');
-const router = express.Router(); 
-const {vistaGatitos, crearGatito} = require('../controller/controller.js')
+const express = require("express");
+const router = express.Router();
+const {
+  vistaGatitos,
+  crearGatito,
+  vistaUnGatito,
+  editarGatito,
+  eliminarGatito,
+} = require("../controller/controller.js");
+const { check, validationResult, body } = require("express-validator");
 
 /* GET users listing. */
-router.get('/ver', vistaGatitos);
-router.post('/crear', crearGatito);
+router.get("/ver", vistaGatitos);
+router.get("/ver/:id", vistaUnGatito);
+router.post(
+  "/crear",
+  [
+    check("name")
+      .not()
+      .isEmpty()
+      .isLength({ max: 15, min: 3 })
+      .withMessage("El nombre debe tener más de 3 y menos de 15 caracteres..."),
+  ],
+  crearGatito
+);
+router.put("/editar/:id", editarGatito);
+router.delete("/eliminar/:id", eliminarGatito);
 
 module.exports = router;
